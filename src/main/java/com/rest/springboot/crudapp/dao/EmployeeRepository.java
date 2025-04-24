99fda9e73ec210577b881bc6beff83ee0bee5ab9
Sounak <sounaksaha333@gmail.com>
2025-04-25 01:33:32 +0530

    spring data rest configs

--- src/main/java/com/rest/springboot/crudapp/dao/EmployeeRepository.java
+++ src/main/java/com/rest/springboot/crudapp/dao/EmployeeRepository.java
@@ -2,6 +2,8 @@ package com.rest.springboot.crudapp.dao;
 
 import com.rest.springboot.crudapp.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
